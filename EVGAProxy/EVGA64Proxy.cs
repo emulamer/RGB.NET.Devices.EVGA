ffc1b4a3fb2fd0f45553a0606912033ab5a0765f
max <max@emulamer.com>
2020-01-03 19:36:45 -0500

    Workie but uglie

---
+++ EVGAProxy/EVGA64Proxy.cs
@@ -0,0 +1,358 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVGAProxy
+{
+    public class EVGA64Proxy
+    {
+        public static void Log(string msg)
+        {
+            try
+            {
+                File.AppendAllText(Path.Combine(Path.GetTempPath(), "evgaproxy.log"), DateTime.Now.ToString() + ": " + (msg ?? "") + "\r\n");
+            }
+            catch {
+                System.Threading.Thread.Sleep(100);
+                try
+                {
+                    File.AppendAllText(Path.Combine(Path.GetTempPath(), "evgaproxy.log"), DateTime.Now.ToString() + ": " + (msg ?? "") + "\r\n");
+                }
+                catch { }
+            }
+        }
+        private string _evgaPath = null;
+        private string _precisionExe = null;
+        private string _precisionExeName = null;
+        private Type _ledInterface;
+        private PropertyInfo _zoneSupported;
+        private PropertyInfo _zoneSet;
+        private MethodInfo _setLEDOff;
+        private MethodInfo _setLEDStaticOn;
+        private MethodInfo _getDefaultColor;
+        private List<object> _instances = new List<object>();
+        public EVGA64Proxy()
+        {
+            try
+            {
+                string assyPath = null;
+                string rootPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                try
+                {
+                    RegistryKey key = null;
+                    try
+                    {
+                        key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64)?.OpenSubKey(@"SOFTWARE\EVGA Precision X1");
+                    }
+                    catch { }
+                    if (key == null)
+                    {
+                        try
+                        {
+                            key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32)?.OpenSubKey(@"SOFTWARE\EVGA Precision X1");
+                        }
+                        catch
+                        { }
+                    }
+                    if (key == null)
+                    {
+                        //todo: bad
+                        throw new Exception("EVGA Precision X1 doesn't seem to be installed (no key)");
+                    }
+                    var dirval = key.GetValue(@"Install_Dir")?.ToString();
+                    if (string.IsNullOrWhiteSpace(dirval))
+                    {
+                        //todo: bad
+                        throw new Exception("EVGA Precision X1 doesn't seem to be installed (no installdir)");
+                    }
+                    _evgaPath = dirval;
+                    var exeName = "PrecisionX_x64";
+                    assyPath = Path.Combine(_evgaPath, exeName + ".exe");
+                    if (!File.Exists(assyPath))
+                    {
+                        exeName = "PrecisionX";
+                        assyPath = Path.Combine(_evgaPath, exeName + ".exe");
+                        if (!File.Exists(assyPath))
+                        {
+                            throw new Exception("PrecisionX.exe or PrecisionX_x64.exe don't seem to exist");
+                        }
+
+                    }
+                    _precisionExeName = exeName;
+                    _precisionExe = assyPath;
+                }
+                catch (Exception ex)
+                {
+                    Log(ex.Message);
+                    //fail
+                    throw;
+                }
+                var dlls = Directory.GetFiles(_evgaPath, "*.dll").ToList();
+                foreach (var dll in dlls)
+                {
+                    try
+                    {
+                        File.Copy(dll, Path.Combine(rootPath, Path.GetFileName(dll)), true);
+                    }
+                    catch (Exception ex)
+                    {
+                        Log($"Failed copying {dll}: {ex.Message}");
+                    }
+                }
+                //var nvFile = Path.Combine(_evgaPath, "ManagedNvApi.dll");
+                
+                //if (!File.Exists(nvFile))
+                //{
+                //    throw new Exception("Unable to find ManagedNvApi.dll");
+                //}
+
+                //try
+                //{
+                //    Log("Finding ManagedNvApi");
+                //    Log(Directory.GetCurrentDirectory());
+                //    var newFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), Path.GetFileName(nvFile));
+                //    bool copy = false;
+                //    if (File.Exists(newFile))
+                //    {
+                //        //comparing file sizes should be enough?
+                //        if (new FileInfo(nvFile).Length != new FileInfo(newFile).Length)
+                //        {
+                //            copy = true;
+
+                //        }
+                //    }
+                //    else
+                //    {
+                //        copy = true;
+                //    }
+                //    if (copy)
+                //    {
+                //        Log("copying managednvapi to " + newFile);
+                //        File.Copy(nvFile, newFile, true);
+                //    }
+                //}
+                //catch (Exception ex)
+                //{
+                //    //failed to copy file
+                //    throw new Exception("Failed to copy ManagedNvApi locally! " + ex.Message, ex);
+                //}
+                var curDir = Directory.GetCurrentDirectory();
+                Directory.SetCurrentDirectory(_evgaPath);
+                AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+                try
+                {
+                    Assembly evgaAssembly = null;
+
+                    try
+                    {
+                        evgaAssembly = Assembly.Load(_precisionExeName);
+                    }
+                    catch (ReflectionTypeLoadException fle)
+                    {
+                        foreach (var g in fle.LoaderExceptions)
+                        {
+                            Log(g.Message);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("Failed to extract EVGA stuff", ex);
+                    }
+
+                    var ledInterface = evgaAssembly.GetType("PX18.Model.ILedCtrl");
+                    if (ledInterface == null)
+                    {
+                        throw new Exception("Failed to find ILedCtrl interface");
+                    }
+                    _ledInterface = ledInterface;
+                    _zoneSet = _ledInterface.GetProperty("ZoneSet");
+                    _setLEDOff = _ledInterface.GetMethod("SetLEDOff");
+                    _setLEDStaticOn = _ledInterface.GetMethod("SetLEDStaticOn");
+                    _getDefaultColor = _ledInterface.GetMethod("GetDefaultColor");
+
+                    _zoneSupported = ledInterface.GetProperty("ZoneSupported");
+
+                    List<Type> ledControls;
+                    try
+                    {
+                        ledControls = evgaAssembly.GetTypes().Where(x => x.IsClass && ledInterface.IsAssignableFrom(x)).ToList();
+                    }
+                    catch (ReflectionTypeLoadException fle)
+                    {
+                        foreach (var g in fle.LoaderExceptions)
+                        {
+                            Log(g.Message);
+                        }
+                        throw;
+                    }
+
+                    foreach (var control in ledControls)
+                    {
+                        var initializedProp = control.GetProperty("IsInitialized");
+                        if (initializedProp == null)
+                        {
+                            //doesn't have IsInitialized
+                            continue;
+                        }
+                        object instance = null;
+                        try
+                        {
+
+                            var con = control.GetConstructor(new Type[] { typeof(uint), typeof(bool) });
+                            if (con == null)
+                            {
+                                con = control.GetConstructor(new Type[] { typeof(uint) });
+                                if (con == null)
+                                {
+                                    //type doesn't have an expected constructor
+                                    continue;
+                                }
+                                else
+                                {
+                                    instance = Activator.CreateInstance(control, new object[] { (uint)0 });
+                                }
+                            }
+                            else
+                            {
+                                instance = Activator.CreateInstance(control, new object[] { (uint)0, false });
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            //exception creating instance
+                        }
+                        if (instance == null)
+                        {
+                            //failed to create instance
+
+                        }
+                        else
+                        {
+                            bool isInitted = (bool)initializedProp.GetValue(instance, null);
+                            if (isInitted && (uint)_zoneSupported.GetValue(instance, null) > 0)
+                            {
+                                _instances.Add(instance);
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    Directory.SetCurrentDirectory(curDir);
+                    AppDomain.CurrentDomain.AssemblyResolve -= CurrentDomain_AssemblyResolve;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log(ex.Message);
+                throw;
+            }
+        }
+
+        public int GetNumberOfDevices()
+        {
+            return _instances.Count();
+        }
+
+        public int GetLedCount(int deviceId)
+        {
+            if (deviceId < 0 || deviceId >= _instances.Count())
+            {
+                return -1;
+            }
+            var zones = (uint)_zoneSupported.GetValue(_instances[deviceId]);
+            int count = 0;
+            for (int i= 0; i< 32; i++)
+            {
+                if (((zones>>i) & 1) == 1)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public void SetColor(int deviceId, int ledId, byte a, byte r, byte g, byte b)
+        {
+            try
+            {
+                if (deviceId < 0 || deviceId >= _instances.Count())
+                {
+                    return;
+                }
+                if (ledId < 0 || ledId > 31)
+                {
+                    return;
+                }
+                var instance = _instances[deviceId];
+                _zoneSet.SetValue(instance, (uint)1 << ledId, null);
+                _setLEDStaticOn.Invoke(instance, new object[] { new System.Windows.Media.Color() { A = a, R = r, B = b, G = g } });
+            }
+            catch { }
+        }
+
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            var name = args.Name.Split(',')[0];
+            var assName = Path.Combine(_evgaPath, name + ".dll");
+            if (!File.Exists(assName))
+            {
+                assName = Path.Combine(_evgaPath, name + ".exe");
+            }
+            if (File.Exists(assName))
+            {
+                try
+                {
+                    var a = Assembly.Load(File.ReadAllBytes(assName));
+                    return a;
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            return LoadFromEvgaResource(name);
+        }
+
+        private Assembly evgaAssembly;
+        private List<string> evgaResources = null;
+        private Assembly LoadFromEvgaResource(string name)
+        {
+            try
+            {
+                if (evgaAssembly == null)
+                {
+                    evgaAssembly = Assembly.ReflectionOnlyLoadFrom(_precisionExe);
+                }
+                if (evgaResources == null)
+                {
+                    evgaResources = new List<string>();
+                    evgaResources.AddRange(evgaAssembly.GetManifestResourceNames());
+                }
+                var found = evgaResources.FirstOrDefault(x => x.ToLower().EndsWith(name.ToLower() + ".dll"));
+                if (found == null)
+                {
+                    return null;
+                }
+                using (var mrs = evgaAssembly.GetManifestResourceStream(found))
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        mrs.CopyTo(ms);
+                        return Assembly.Load(ms.ToArray());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ RGB.NET.Devices.EVGA/EVGADeviceProvider.cs
@@ -0,0 +1,249 @@
+﻿using Microsoft.Win32;
+using RGB.NET.Core;
+using RGB.NET.Devices.EVGA.Generic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace RGB.NET.Devices.EVGA
+{
+    public class EVGADeviceProvider : IRGBDeviceProvider
+    {
+        private string _evgaPath = null;
+        private string _precisionExe = null;
+        private string _precisionExeName = null;
+        private Type _comInterface;
+        private Process _proxyProc;
+        private NamedPipeClientStream nps;
+        private static byte[] MAGIC = new byte[] { 0xB, 0xE, 0xE, 0xF };
+        private byte[] ReadMsg(Stream nps)
+        {
+            byte[] magic = new byte[4];
+            int readlen = nps.Read(magic, 0, 4);
+            if (readlen != 4 || !(magic[0] == 0xB && magic[1] == 0xE && magic[2] == 0xE && magic[3] == 0xF))
+            {
+                throw new Exception("No beef");
+            }
+
+            byte[] bMsg = new byte[16];
+            readlen = nps.Read(bMsg, 0, bMsg.Length);
+            if (readlen != 16)
+            {
+                throw new Exception("Bad data");
+            }
+
+            return bMsg;
+        }
+        private int GetDeviceCount()
+        {
+            nps.Write(MAGIC, 0, 4);
+            byte[] bMsg = new byte[16];
+            bMsg[0] = 1;
+            nps.Write(bMsg, 0, bMsg.Length);
+            bMsg = ReadMsg(nps);
+            if (bMsg[0] != 2)
+            {
+                throw new Exception("Wrong response");
+            }
+            return bMsg[1];
+        }
+
+        private int GetLedCount(int deviceId)
+        {
+            nps.Write(MAGIC, 0, 4);
+            var bMsg = new byte[16];
+            bMsg[0] = 5;
+            bMsg[1] = (byte)deviceId;
+            nps.Write(bMsg, 0, bMsg.Length);
+            bMsg = ReadMsg(nps);
+            if (bMsg[0] != 6)
+            {
+                throw new Exception("Wrong response");
+            }
+            return bMsg[1];
+        }
+        public static void Log(string msg)
+        {
+            try
+            {
+                File.AppendAllText(Path.Combine(Path.GetTempPath(), "evgaproxy.log"), DateTime.Now.ToString() + ": " + (msg ?? "") + "\r\n");
+            }
+            catch
+            {
+                System.Threading.Thread.Sleep(100);
+                try
+                {
+                    File.AppendAllText(Path.Combine(Path.GetTempPath(), "evgaproxy.log"), DateTime.Now.ToString() + ": " + (msg ?? "") + "\r\n");
+                }
+                catch { }
+            }
+        }
+        internal void SetLed(int deviceId, int ledId, byte a, byte r, byte g, byte b)
+        {
+            var bMsg = new byte[16];
+            bMsg[0] = 10;
+            bMsg[1] = (byte)deviceId;
+            bMsg[2] = (byte)ledId;
+            bMsg[3] = a;
+            bMsg[4] = r;
+            bMsg[5] = g;
+            bMsg[6] = b;
+            nps.Write(MAGIC, 0, 4);
+            nps.Write(bMsg, 0, bMsg.Length);
+        }
+
+        public EVGADeviceProvider()
+        {
+            Log("Constructor hit");
+            try
+            {
+                Log("Trying to make temp folder");
+                var tempFolder = Path.Combine(Path.GetTempPath(), "evgaproxy");
+                if (!Directory.Exists(tempFolder))
+                {
+                    Directory.CreateDirectory(tempFolder);
+                }
+                try
+                {
+                    string proxyPath = Path.Combine(tempFolder, "EVGAProxy.exe");
+                    using (var s = Assembly.GetExecutingAssembly().GetManifestResourceStream("RGB.NET.Devices.EVGA.EVGAProxy.exe"))
+                    {
+                        using (var fs = File.Open(proxyPath, FileMode.Create))
+                        {
+                            s.CopyTo(fs);
+                        }
+                    }
+                    ProcessStartInfo psi = new ProcessStartInfo(proxyPath) { UseShellExecute = true, WorkingDirectory = tempFolder, CreateNoWindow = true };
+                    _proxyProc = Process.Start(psi);
+                }
+                catch (Exception ex)
+                {
+                    //couldn't write it, maybe it's already there and running?
+                }
+                nps = new NamedPipeClientStream(".", "evgargbled", PipeDirection.InOut);
+                try
+                {
+                    nps.Connect(5000);
+                }
+                catch
+                {
+                    throw new Exception("Coudln't connect to EVGAProxy");
+                }
+                EVGADeviceInfo devInfo = new EVGADeviceInfo();
+                _devices.Clear();
+
+                var numDevs = GetDeviceCount();
+                for (int i = 0; i < numDevs; i++)
+                {
+                    int ledCount = GetLedCount(i);
+                    _devices.Add(new EVGADevice(devInfo, SetLed, (uint)i, (uint)ledCount));
+                }
+            }
+            catch (Exception ex)
+            {
+                Log($"Exception in rgb.net: {ex.Message}");
+                throw;
+            }
+        }
+
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            var name = args.Name.Split(',')[0];
+            var assName = Path.Combine(_evgaPath, name + ".dll");
+            if (!File.Exists(assName))
+            {
+                assName = Path.Combine(_evgaPath, name + ".exe");
+            }
+            if (File.Exists(assName))
+            {
+                try
+                {
+                    var a = Assembly.Load(File.ReadAllBytes(assName));
+                    return a;
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            return LoadFromEvgaResource(name);
+        }
+
+        private Assembly evgaAssembly;
+        private List<string> evgaResources = null;
+        private Assembly LoadFromEvgaResource(string name)
+        {
+            try
+            {
+                if (evgaAssembly == null)
+                {
+                    evgaAssembly = Assembly.ReflectionOnlyLoadFrom(_precisionExe);
+                }
+                if (evgaResources == null)
+                {
+                    evgaResources = new List<string>();
+                    evgaResources.AddRange(evgaAssembly.GetManifestResourceNames());
+                }
+                var found = evgaResources.FirstOrDefault(x => x.ToLower().EndsWith(name.ToLower() + ".dll"));
+                if (found == null)
+                {
+                    return null;
+                }
+                using (var mrs = evgaAssembly.GetManifestResourceStream(found))
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        mrs.CopyTo(ms);
+                        return Assembly.Load(ms.ToArray());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        private static EVGADeviceProvider _instance;
+        
+        public static EVGADeviceProvider Instance => _instance ?? new EVGADeviceProvider();
+
+        public bool IsInitialized => true;
+
+        private List<IRGBDevice> _devices = new List<IRGBDevice>();
+        public IEnumerable<IRGBDevice> Devices => _devices;
+
+        public bool HasExclusiveAccess => false;
+
+        public void Dispose()
+        {
+            if (_proxyProc != null)
+            {
+                try
+                {
+                    _proxyProc.Kill();
+                } catch
+                { }
+                finally
+                {
+                    _proxyProc = null;
+                }
+            }
+        }
+
+        public bool Initialize(RGBDeviceType loadFilter = (RGBDeviceType)(-1), bool exclusiveAccessIfPossible = false, bool throwExceptions = false)
+        {
+            return true;
+        }
+
+        public void ResetDevices()
+        {
+            
+        }
+    }
+}
